04b5453a61daa61a99a19cc8c71edcf3f8ec8780
Lilyana <lilyana.ihtimanska@gmail.com>
2018-02-17 16:33:19 +0200

    added views, controllers and services for book and category

---
+++ grails-app/controllers/library/items/CategoryController.groovy
@@ -0,0 +1,109 @@
+package library.items
+
+import grails.plugin.springsecurity.annotation.Secured
+import grails.validation.ValidationException
+import static org.springframework.http.HttpStatus.*
+
+@SuppressWarnings(['FactoryMethodName', 'ReturnNullFromCatchBlock'])
+class CategoryController {
+
+    CategoryService categoryService
+
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    @Secured(['ROLE_ADMIN'])
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond categoryService.list(params), model:[categoryCount: categoryService.count()]
+    }
+
+    @Secured(['ROLE_ADMIN'])
+    def show(Long id) {
+        respond categoryService.get(id)
+    }
+
+    @Secured(['ROLE_ADMIN'])
+    def create() {
+        respond new Category(params)
+    }
+
+    @Secured(['ROLE_ADMIN'])
+    def save(Category category) {
+        if (category == null) {
+            notFound()
+            return
+        }
+
+        try {
+            categoryService.save(category)
+        } catch (ValidationException e) {
+            respond category.errors, view:'create'
+            return
+        }
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'category.label', default: 'Category'), category.id])
+                redirect category
+            }
+            '*' { respond category, [status: CREATED] }
+        }
+    }
+
+    @Secured(['ROLE_ADMIN'])
+    def edit(Long id) {
+        respond categoryService.get(id)
+    }
+
+    @Secured(['ROLE_ADMIN'])
+    def update(Category category) {
+        if (category == null) {
+            notFound()
+            return
+        }
+
+        try {
+            categoryService.save(category)
+        } catch (ValidationException e) {
+            respond category.errors, view:'edit'
+            return
+        }
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'category.label', default: 'Category'), category.id])
+                redirect category
+            }
+            '*'{ respond category, [status: OK] }
+        }
+    }
+
+    @Secured(['ROLE_ADMIN'])
+    def delete(Long id) {
+        if (id == null) {
+            notFound()
+            return
+        }
+
+        categoryService.delete(id)
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'category.label', default: 'Category'), id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    @Secured(['ROLE_ADMIN'])
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'category.label', default: 'Category'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
+}
--- grails-app/init/library/BootStrap.groovy
+++ grails-app/init/library/BootStrap.groovy
@@ -6,7 +6,7 @@ class BootStrap {
 
     def init = { servletContext ->
 
-        def authorities = ['ROLE_BOSS', 'ROLE_EMPLOYEE']
+        def authorities = ['ROLE_BOSS', 'ROLE_EMPLOYEE', 'ROLE_ADMIN']
         authorities.each { String authority ->
             if ( !SecurityRole.findByAuthority(authority) ) {
                 new SecurityRole(authority: authority).save()
@@ -23,6 +23,12 @@ class BootStrap {
             def u = new User(username: 'watson', password: '221Bbakerstreet')
             u.save()
             new UserSecurityRole(user: u, securityRole: SecurityRole.findByAuthority('ROLE_EMPLOYEE')).save()
+        }
+
+        if ( !User.findByUsername('lilyana') ) {
+            def u = new User(username: 'lilyana', password: 'lilyanapass')
+            u.save()
+            new UserSecurityRole(user: u, securityRole: SecurityRole.findByAuthority('ROLE_ADMIN')).save()
         }
     }
     def destroy = {
