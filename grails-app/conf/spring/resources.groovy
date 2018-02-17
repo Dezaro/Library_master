1bac68851be906ba36d67317b75132d0941eb9bb
Dezaro <delobranchev@gamil.com>
2018-02-17 12:53:51 +0200

    Initial commit

---
+++ grails-app/conf/spring/resources.groovy
@@ -0,0 +1,5 @@
+import library.items.UserPasswordEncoderListener
+// Place your Spring DSL code here
+beans = {
+    userPasswordEncoderListener(UserPasswordEncoderListener, ref('hibernateDatastore'))
+}
---
+++ grails-app/init/library/BootStrap.groovy
@@ -0,0 +1,30 @@
+package library
+
+import library.items.*
+
+class BootStrap {
+
+    def init = { servletContext ->
+
+        def authorities = ['ROLE_BOSS', 'ROLE_EMPLOYEE']
+        authorities.each { String authority ->
+            if ( !SecurityRole.findByAuthority(authority) ) {
+                new SecurityRole(authority: authority).save()
+            }
+        }
+
+        if ( !User.findByUsername('sherlock') ) {
+            def u = new User(username: 'sherlock', password: 'elementary')
+            u.save()
+            new UserSecurityRole(user: u, securityRole: SecurityRole.findByAuthority('ROLE_BOSS')).save()
+        }
+
+        if ( !User.findByUsername('watson') ) {
+            def u = new User(username: 'watson', password: '221Bbakerstreet')
+            u.save()
+            new UserSecurityRole(user: u, securityRole: SecurityRole.findByAuthority('ROLE_EMPLOYEE')).save()
+        }
+    }
+    def destroy = {
+    }
+}
