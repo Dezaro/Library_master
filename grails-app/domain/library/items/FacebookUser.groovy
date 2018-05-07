4ba40298074772050d317c1615157fe07fa2ab44
Dezaro <delobranchev@gamil.com>
2018-05-07 17:54:22 +0300

    google login in progres

---
+++ grails-app/domain/library/items/FacebookUser.groovy
@@ -0,0 +1,14 @@
+package library.items
+
+class FacebookUser {
+
+    Long uid
+    String accessToken
+    Date accessTokenExpires
+
+    static belongsTo = [user: User] //connected to main Spring Security domain
+
+    static constraints = {
+        uid unique: true
+    }
+}
---
+++ src/test/groovy/library/items/FacebookUserSpec.groovy
@@ -0,0 +1,18 @@
+package library.items
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class FacebookUserSpec extends Specification implements DomainUnitTest<FacebookUser> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
