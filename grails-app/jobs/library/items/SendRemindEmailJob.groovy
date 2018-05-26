e48a573dae52b04f2f308a51035f03daf3d64249
Lilyana <lilyana.ihtimanska@gmail.com>
2018-05-26 10:40:36 +0300

    Cron job is done

--- grails-app/jobs/library/items/SendRemindEmailJob.groovy
+++ grails-app/jobs/library/items/SendRemindEmailJob.groovy
@@ -1,46 +1,31 @@
 package library.items
 
-class SendRemindEmailJob {
+import grails.plugins.mail.MailService
 
-    UserService userService
-    EmailSenderController emailSender
+class SendRemindEmailJob {
+    MailService mailService
     RentBookService rentBookService
+
     static triggers = {
-      simple repeatInterval: 10000l // execute job once in 5 seconds
-//        cron name: 'sendRemindEmail', cronExpression: "0 0 6 * * ?"
+//      simple repeatInterval: 10000l // execute job once in 10 seconds
+        cron name: 'sendRemindEmail', cronExpression: "0 15 10 ? * MON,WED,FRI"
     }
 
     def execute() {
         def rentBooks = rentBookService.list()
         def today = new Date()
-//        def readerCard
-//        def userEmail
         rentBooks.each {
             rentBook ->
              if(!rentBook.isReturn && rentBook.returnBeforeDate - 3 <= today){
                  print(rentBook.getReaderCard().email)
-//                 emailSender.send().params = [
-//                     email:rentBook.getReaderCard().email,
-//                     name: rentBook.getReaderCard().readerNames,
-//                     book: rentBook.getBookItem().getBook().title,
-//                     returnBeforeDate: rentBook.returnBeforeDate
-//                 ]
-//                 redirect(controller: "emailSender", action: "send",
-//                         params: [email:rentBook.getReaderCard().email,
-//                                 name:rentBook.getReaderCard().readerNames,
-//                                 book:rentBook.getBookItem().getBook().title,
-//                                returnBeforeDate:rentBook.returnBeforeDate]
-//                 )
-                 sendMail {
+                 mailService.sendMail{
                      async true
                      to rentBook.getReaderCard().email
                      subject 'Remind to return book'
                      html '<p>Hello ' + rentBook.getReaderCard().readerNames + ',</p> <p>we remind you that you have to return the book <span style="color:#ff0000; font-weight: bold">' + rentBook.getBookItem().getBook().title + '</span> to date <span style="color:#ff0000; font-weight: bold">' + rentBook.returnBeforeDate + '</span>.</p> <p>Best regards,</p> <p> Library</p>'
                  }
-//                 respond([message: 'Message sent at ' + new Date()], formats: ['json'])
              }
         }
         print("Job is run")
-        // execute job
     }
 }
